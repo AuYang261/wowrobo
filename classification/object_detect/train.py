5a3c70ddacb9f0c5b7f62a517adcc42c99e0640c
AuYang <xu_jyang@stu.pku.edu.cn>
2025-09-26 16:28:10 +0800

    添加YOLO训练代码

---
+++ classification/object_detect/train.py
@@ -0,0 +1,21 @@
+import warnings
+import os
+
+warnings.filterwarnings("ignore")
+from ultralytics import YOLO
+
+if __name__ == "__main__":
+    model = YOLO("ultralytics/cfg/models/11/yolo11m-obb.yaml")
+    # model.load('yolov11.pt') # loading pretrain weights
+    model.train(
+        data=os.path.join(os.path.dirname(__file__), "data.yaml"),
+        cache=False,
+        imgsz=640,
+        epochs=200,
+        batch=16,
+        close_mosaic=10,
+        device="0",
+        optimizer="SGD",  # using SGD
+        project=os.path.join(os.path.dirname(__file__), "runs/train"),
+        name="exp",
+    )
